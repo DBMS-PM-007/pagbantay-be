37a24590eb16d19f71741c4c64758a8946ec980a
ThatDot <justinsanveloso@gmail.com>
2025-04-06 11:18:48 +0800

    feat: added sslrootcert param in DATABASE_URL in database.py

--- database.py
+++ database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ['DATABASE_URL'].replace("postgresql://", "cockroachdb://")
-engine = create_engine(DATABASE_URL)
-engine.connect()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-print("Connected successfully!")
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set!")
+
+DATABASE_URL += "&sslrootcert=./root.crt"
+DATABASE_URL = DATABASE_URL.replace("postgresql://", "cockroachdb://")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+print("Successfully Connected!")
