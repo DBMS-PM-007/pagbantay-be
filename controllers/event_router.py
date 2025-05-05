d1290e5a07772dda9b8c3b5c60a07d9fb97171e3
attmarvy <marvierulida@gmail.com>
2025-05-06 00:32:02 +0800

    feat.git api for specific event

--- controllers/event_router.py
+++ controllers/event_router.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import Session
 from models.event import EventCreate, EventResponse
 from usecases.event_usecase import EventUseCase
 from db.database import get_db
-from models.base import Event
-from uuid import UUID
 from fastapi import HTTPException 
 
 event_router = APIRouter(tags=["Events"])
@@ -21,8 +19,5 @@ def get_events(usecase: EventUseCase = Depends(get_event_usecase)):
     return usecase.get_all_events()
 
 @event_router.get("/{event_id}", response_model=EventResponse)
-def get_event_by_id(event_id: str, db: Session = Depends(get_db)):
-    event = db.query(Event).filter(Event.event_id == event_id).first()
-    if not event:
-        raise HTTPException(status_code=404, detail="Event not found")
-    return event
+def get_event_by_id(event_id: str, usecase: EventUseCase = Depends(get_event_usecase)):
+    return usecase.get_event_by_id(event_id)
