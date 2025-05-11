997ce10fc149a98d55f8df509b18dc1daeb045d4
ThatDot <justinsanveloso@gmail.com>
2025-05-12 04:15:13 +0800

    refactor: moved HTTP exception for Event Availability from Router to Usecase layer

--- usecases/event_availability_usecase.py
+++ usecases/event_availability_usecase.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 from models.event_availability import EventAvailabilityCreate, EventAvailabilityResponse, EventAvailabilityUpdate
 from repositories.event_availability_repository import EventAvailabilityRepository
 
@@ -8,7 +9,6 @@ class EventAvailabilityUseCase:
 
     def create_event_availability(self, availability_data: EventAvailabilityCreate):
         db_availability = self.repo.create_event_availability(availability_data)
-
         return EventAvailabilityResponse.model_validate(db_availability)
 
     def get_event_availability(self):
@@ -17,7 +17,7 @@ class EventAvailabilityUseCase:
     def update_event_availability(self, user_id: str, event_id: str, availability_data: EventAvailabilityUpdate):
         db_availability = self.repo.update_event_availability(user_id, event_id, availability_data)
 
-        if db_availability:
-            return EventAvailabilityResponse.model_validate(db_availability)
-        return None
+        if not db_availability:
+            raise HTTPException(status_code=404, detail="Event availability not found")
 
+        return EventAvailabilityResponse.model_validate(db_availability)
